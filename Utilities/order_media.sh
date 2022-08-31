c72bd834c70f8e5a58dbbf81f119836015b709b7
anantharaj87 <anantharaj.cs@gmail.com>
2022-08-31 21:58:52 +0530

    order media

---
+++ Utilities/order_media.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#inputdir=/media/pi/7A81-86F7/Ownuse/RashmiLive
+#outputdir=/media/pi/7A81-86F7/Ownuse/Personal/Ordered
+
+#./order_media.sh /media/pi/7A81-86F7/Ownuse/NewSyncRashmi /media/pi/7A81-86F7/Ownuse/Personal/Ordered
+
+inputdir=$1
+outputdir=$2
+
+ll=($(find $inputdir -maxdepth 1 -type f -exec echo "{}" \; | grep -Ei ".mp4$"))
+
+for (( idx=0 ; idx<${#ll[@]} ; idx++ ));
+do
+    mnthyr=$(ffprobe -v quiet -show_format ${ll[$idx]} | grep -Eoi "creation_time=[0-9][0-9][0-9][0-9]-[0-9][0-9]" | grep -Eoi "[0-9][0-9][0-9][0-9]-[0-9][0-9]" | tail -1)
+
+
+    if [[ "$mnthyr" == "" ]]
+    then
+        echo ${ll[$idx]} - "meta empty"
+    else
+
+        yr=$(echo $mnthyr | grep -Eoi "[0-9]{4}")
+        echo mv ${ll[$idx]} $outputdir/$yr/$mnthyr/
+
+        mkdir -p $outputdir/$yr/$mnthyr/
+        mv ${ll[$idx]} $outputdir/$yr/$mnthyr/
+    fi
+
+done
+
+ll=($(find $inputdir -maxdepth 1 -type f -exec echo "{}" \; | grep -Ei ".jpg$"))
+
+for (( idx=0 ; idx<${#ll[@]} ; idx++ ));
+do
+    mnthyr=$(exiftool ${ll[$idx]} | grep -Eio "Create Date.*" | grep -Eio "[0-9][0-9][0-9][0-9]:[0-9][0-9]" | tr ':' '-' | tail -1)
+
+
+    if [[ "$mnthyr" == "" ]]
+    then
+        echo ${ll[$idx]} - "meta empty"
+    else
+        yr=$(echo $mnthyr | grep -Eoi "[0-9]{4}")
+        echo mv ${ll[$idx]} $outputdir/$yr/$mnthyr/
+
+        mkdir -p $outputdir/$yr/$mnthyr/
+        mv ${ll[$idx]} $outputdir/$yr/$mnthyr/
+    fi
+
+done
+
+ll=($(find $inputdir -maxdepth 1 -type f -exec echo "{}" \; | grep -Ei ".jpg$|.jpeg$|.mp4$"))
+
+for (( idx=0 ; idx<${#ll[@]} ; idx++ ));
+do
+
+    mnthyr=$(echo ${ll[$idx]} | grep -Eio "[_-][0-9]{8}[_-]" | grep -Eio "[0-9]{8}" | grep -Eio "[0-9]{6}")
+
+
+    if [[ "$mnthyr" == "" ]]
+    then
+        echo ${ll[$idx]} - "meta empty"
+    else
+        mnthyr=$(echo ${mnthyr:0:4}-${mnthyr:4:2})
+
+        yr=$(echo $mnthyr | grep -Eoi "[0-9]{4}")
+        echo mv ${ll[$idx]} $outputdir/$yr/$mnthyr/
+
+        mkdir -p $outputdir/$yr/$mnthyr/
+        mv ${ll[$idx]} $outputdir/$yr/$mnthyr/
+    fi
+
+done
+
