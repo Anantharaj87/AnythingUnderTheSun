07eb338e784e597b5ce9d45df8e94ba8ec67ac1f
anantharaj87 <anantharaj.cs@gmail.com>
2022-08-31 22:13:01 +0530

    order media minor update

--- Utilities/order_media.sh
+++ Utilities/order_media.sh
@@ -5,66 +5,66 @@
 inputdir=$1
 outputdir=$2
 
-ll=($(find $inputdir -maxdepth 1 -type f -exec echo "{}" \; | grep -Ei ".mp4$"))
+filenames=($(find $inputdir -maxdepth 1 -type f -exec echo "{}" \; | grep -Ei ".mp4$"))
 
-for (( idx=0 ; idx<${#ll[@]} ; idx++ ));
+for (( idx=0 ; idx<${#filenames[@]} ; idx++ ));
 do
-    mnthyr=$(ffprobe -v quiet -show_format ${ll[$idx]} | grep -Eoi "creation_time=[0-9][0-9][0-9][0-9]-[0-9][0-9]" | grep -Eoi "[0-9][0-9][0-9][0-9]-[0-9][0-9]" | tail -1)
+    mnthyr=$(ffprobe -v quiet -show_format ${filenames[$idx]} | grep -Eoi "creation_time=[0-9][0-9][0-9][0-9]-[0-9][0-9]" | grep -Eoi "[0-9][0-9][0-9][0-9]-[0-9][0-9]" | tail -1)
 
 
     if [[ "$mnthyr" == "" ]]
     then
-        echo ${ll[$idx]} - "meta empty"
+        echo ${filenames[$idx]} - "meta empty"
     else
 
         yr=$(echo $mnthyr | grep -Eoi "[0-9]{4}")
-        echo mv ${ll[$idx]} $outputdir/$yr/$mnthyr/
+        echo mv ${filenames[$idx]} $outputdir/$yr/$mnthyr/
 
         mkdir -p $outputdir/$yr/$mnthyr/
-        mv ${ll[$idx]} $outputdir/$yr/$mnthyr/
+        mv ${filenames[$idx]} $outputdir/$yr/$mnthyr/
     fi
 
 done
 
-ll=($(find $inputdir -maxdepth 1 -type f -exec echo "{}" \; | grep -Ei ".jpg$"))
+filenames=($(find $inputdir -maxdepth 1 -type f -exec echo "{}" \; | grep -Ei ".jpg$"))
 
-for (( idx=0 ; idx<${#ll[@]} ; idx++ ));
+for (( idx=0 ; idx<${#filenames[@]} ; idx++ ));
 do
-    mnthyr=$(exiftool ${ll[$idx]} | grep -Eio "Create Date.*" | grep -Eio "[0-9][0-9][0-9][0-9]:[0-9][0-9]" | tr ':' '-' | tail -1)
+    mnthyr=$(exiftool ${filenames[$idx]} | grep -Eio "Create Date.*" | grep -Eio "[0-9][0-9][0-9][0-9]:[0-9][0-9]" | tr ':' '-' | tail -1)
 
 
     if [[ "$mnthyr" == "" ]]
     then
-        echo ${ll[$idx]} - "meta empty"
+        echo ${filenames[$idx]} - "meta empty"
     else
         yr=$(echo $mnthyr | grep -Eoi "[0-9]{4}")
-        echo mv ${ll[$idx]} $outputdir/$yr/$mnthyr/
+        echo mv ${filenames[$idx]} $outputdir/$yr/$mnthyr/
 
         mkdir -p $outputdir/$yr/$mnthyr/
-        mv ${ll[$idx]} $outputdir/$yr/$mnthyr/
+        mv ${filenames[$idx]} $outputdir/$yr/$mnthyr/
     fi
 
 done
 
-ll=($(find $inputdir -maxdepth 1 -type f -exec echo "{}" \; | grep -Ei ".jpg$|.jpeg$|.mp4$"))
+filenames=($(find $inputdir -maxdepth 1 -type f -exec echo "{}" \; | grep -Ei ".jpg$|.jpeg$|.mp4$"))
 
-for (( idx=0 ; idx<${#ll[@]} ; idx++ ));
+for (( idx=0 ; idx<${#filenames[@]} ; idx++ ));
 do
 
-    mnthyr=$(echo ${ll[$idx]} | grep -Eio "[_-][0-9]{8}[_-]" | grep -Eio "[0-9]{8}" | grep -Eio "[0-9]{6}")
+    mnthyr=$(echo ${filenames[$idx]} | grep -Eio "[_-][0-9]{8}[_-]" | grep -Eio "[0-9]{8}" | grep -Eio "[0-9]{6}")
 
 
     if [[ "$mnthyr" == "" ]]
     then
-        echo ${ll[$idx]} - "meta empty"
+        echo ${filenames[$idx]} - "meta empty"
     else
         mnthyr=$(echo ${mnthyr:0:4}-${mnthyr:4:2})
 
         yr=$(echo $mnthyr | grep -Eoi "[0-9]{4}")
-        echo mv ${ll[$idx]} $outputdir/$yr/$mnthyr/
+        echo mv ${filenames[$idx]} $outputdir/$yr/$mnthyr/
 
         mkdir -p $outputdir/$yr/$mnthyr/
-        mv ${ll[$idx]} $outputdir/$yr/$mnthyr/
+        mv ${filenames[$idx]} $outputdir/$yr/$mnthyr/
     fi
 
 done
