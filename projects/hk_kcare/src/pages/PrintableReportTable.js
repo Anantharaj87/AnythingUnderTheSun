4fb42e2491f187c480f38bb48866d0fcfd1fb4b9
Anantharaj87 <anantharaj.cs@gmail.com>
2023-06-27 20:21:19 +0530

    relative bootstrap for pdf

--- projects/hk_kcare/src/pages/PrintableReportTable.js
+++ projects/hk_kcare/src/pages/PrintableReportTable.js
@@ -110,8 +110,7 @@ const prepareContent = () => {
     
     <html>
 <head>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-
+<link href="./node_modules/bootstrap/dist/css/bootstrap.css" />
 </head>
 <body>
 
