0d0da9ad2dedc006b7860b14eba737b398d776f3
Anantharaj87 <anantharaj.cs@gmail.com>
2023-06-27 15:54:16 +0530

    minor

--- projects/hk_kcare/src/pages/Lab.js
+++ projects/hk_kcare/src/pages/Lab.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import PrintableReportTable from "./PrintableReportTable";
 import ReportInputTable_plain from "./ReportInputTable_plain";
 //import './Lab.css';
+import { saveAs } from 'file-saver';
 
 function Lab(props) {
 
